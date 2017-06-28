228d8560a226f3d87eb578b0a68c9c004cb3e7da
Sanders Lauture <golf1052@msn.com>
2017-06-27 22:57:06 -0700

    Add flux mode

--- Huemongous/LightPage.xaml.cs
+++ Huemongous/LightPage.xaml.cs
@@ -1,9 +1,13 @@
-﻿using Q42.HueApi;
+﻿using Newtonsoft.Json.Linq;
+using Q42.HueApi;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,9 +29,15 @@ namespace Huemongous
     {
         Light light;
         bool justLoaded = true;
+        HttpClient httpClient;
+        DateTime sunrise;
+        DateTime sunset;
+
         public LightPage()
         {
             this.InitializeComponent();
+
+            httpClient = new HttpClient();
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -35,7 +45,60 @@ namespace Huemongous
             light = await AppConstants.HueClient.GetLightAsync((string)e.Parameter);
             lightSwitch.IsOn = light.State.On;
             justLoaded = false;
+            await GetTimeOfDay();
+            UpdateLight();
             base.OnNavigatedTo(e);
+        }
+
+        async Task GetTimeOfDay()
+        {
+            Geolocator geolocator = new Geolocator();
+            geolocator.AllowFallbackToConsentlessPositions();
+            Geoposition geoposition = await geolocator.GetGeopositionAsync();
+            HttpResponseMessage astronomyResponse = await httpClient.GetAsync(
+                    $"http://api.wunderground.com/api/{Secrets.WundergroundApiKey}/astronomy/q/{geoposition.Coordinate.Point.Position.Latitude},{geoposition.Coordinate.Point.Position.Longitude}.json");
+            JObject o = JObject.Parse(await astronomyResponse.Content.ReadAsStringAsync());
+            sunrise = DateTimeHelper(int.Parse((string)o["moon_phase"]["sunrise"]["hour"]), int.Parse((string)o["moon_phase"]["sunrise"]["minute"]));
+            sunset = DateTimeHelper(int.Parse((string)o["moon_phase"]["sunset"]["hour"]), int.Parse((string)o["moon_phase"]["sunset"]["minute"]));
+        }
+
+        async Task UpdateLight()
+        {
+            DateTime startTime = sunset - TimeSpan.FromMinutes(30);
+            DateTime endTime = sunset + TimeSpan.FromMinutes(30);
+            while (DateTime.Now >= startTime && DateTime.Now <= endTime)
+            {
+                int startingTemp = 6500;
+                int endingTemp = 2000;
+                TimeSpan transitionTime = TimeSpan.FromHours(1);
+                double amountPerMin = (startingTemp - endingTemp) / transitionTime.TotalMinutes;
+                double amount = (DateTime.Now - startTime).TotalMinutes * amountPerMin;
+                double desiredTemp = startingTemp - amount;
+                int mired = GetMired((int)desiredTemp);
+                LightCommand command = new LightCommand();
+                command.ColorTemperature = mired;
+                command.TransitionTime = TimeSpan.FromSeconds(60);
+                AppConstants.HueClient.SendCommandAsync(command, new List<string> { light.Id });
+                await Task.Delay(TimeSpan.FromMinutes(1));
+            }
+            if (DateTime.Now > endTime)
+            {
+                LightCommand command = new LightCommand();
+                command.ColorTemperature = GetMired(2000);
+                AppConstants.HueClient.SendCommandAsync(command, new List<string> { light.Id });
+            }
+            else if (DateTime.Now < startTime)
+            {
+                LightCommand command = new LightCommand();
+                command.ColorTemperature = GetMired(6500);
+                AppConstants.HueClient.SendCommandAsync(command, new List<string> { light.Id });
+            }
+        }
+
+        private DateTime DateTimeHelper(int hour, int minute)
+        {
+            DateTime now = DateTime.Now;
+            return new DateTime(now.Year, now.Month, now.Day, hour, minute, 0);
         }
 
         private void fluxRadioButton_Checked(object sender, RoutedEventArgs e)
@@ -71,10 +134,15 @@ namespace Huemongous
                 return;
             }
             int selectedTemp = int.Parse((string)((ComboBoxItem)tempComboBox.SelectedItem).Tag);
-            int mired = 1000000 / selectedTemp;
+            int mired = GetMired(selectedTemp);
             LightCommand command = new LightCommand();
             command.ColorTemperature = mired;
             AppConstants.HueClient.SendCommandAsync(command, new List<string>() { light.Id });
+        }
+
+        int GetMired(int temp)
+        {
+            return 1000000 / temp;
         }
     }
 }
